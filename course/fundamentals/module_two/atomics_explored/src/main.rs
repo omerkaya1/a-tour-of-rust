7767e37a2771472902df3440d277c3590a721402
Omer Kaya <omerkaya513@gmail.com>
2024-12-29 14:18:27 +0200

    [add]: atomics explored
    - added an atomic to the programme.

--- course/fundamentals/module_two/atomics_explored/src/main.rs
+++ course/fundamentals/module_two/atomics_explored/src/main.rs
@@ -1,3 +1,20 @@
+use std::sync::atomic::AtomicI32;
+
+static STATE: AtomicI32 = AtomicI32::new(0);
+
 fn main() {
-    println!("Hello, world!");
+    let mut handles = Vec::new();
+
+    for _ in 0..1000 {
+        let handle = std::thread::spawn(|| {
+            for _ in 0..1100 {
+                STATE.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+            }
+        });
+
+        handles.push(handle);
+    }
+
+    handles.into_iter().for_each(|h| h.join().unwrap());
+    println!("{}", STATE.load(std::sync::atomic::Ordering::Relaxed));
 }
