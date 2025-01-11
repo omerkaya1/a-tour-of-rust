ef062e24c15f1d093863db5319786d3bf782db2f
Omer Kaya <omerkaya513@gmail.com>
2025-01-11 15:31:15 +0200

    [add]: simple tcp client

--- course/fundamentals/module_three/tcpserver/src/main.rs
+++ course/fundamentals/module_three/tcpserver/src/main.rs
@@ -1,11 +1,37 @@
+use std::time::Duration;
+
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
     spawn,
 };
 
+async fn tcp_client() -> anyhow::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:8080").await?;
+    println!("connected to the server");
+
+    // prefixing a string with 'b' allows for a shorthand conversion of a string into a byte collection
+    stream.write_all(b"some data").await?;
+
+    let mut buf = vec![0u8; 1024];
+    let bytes_read = stream.read(&mut buf).await?;
+
+    println!("Response {}", String::from_utf8_lossy(&buf[..bytes_read]));
+    Ok(())
+}
+
+async fn client_runner() {
+    loop {
+        tokio::time::sleep(Duration::from_millis(10)).await;
+        let _ = tcp_client().await;
+    }
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // spawn a tcp client routine
+    tokio::spawn(client_runner());
+
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
     loop {
@@ -17,9 +43,9 @@ async fn main() -> anyhow::Result<()> {
 
             loop {
                 let n = socket
-                .read(&mut buf)
-                .await
-                .expect("failed to read data from socket");
+                    .read(&mut buf)
+                    .await
+                    .expect("failed to read data from socket");
 
                 if n == 0 {
                     return;
@@ -31,5 +57,5 @@ async fn main() -> anyhow::Result<()> {
                     .expect("failed to write data to socket");
             }
         });
-    };
+    }
 }
