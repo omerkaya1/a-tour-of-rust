84bfc3c070ca2e82ab1f615b54d997a8b6860d06
Omer Kaya <omerkaya513@gmail.com>
2025-01-14 15:36:15 +0200

    [add]: json serialisable response with axum

--- course/fundamentals/module_three/tokio_axum/src/main.rs
+++ course/fundamentals/module_three/tokio_axum/src/main.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use axum::{response::Html, routing::get, Router};
+use serde::Serialize;
 
 async fn say_hello() -> Html<&'static str> {
     Html("<h1>Some hello in HTML</h1>")
@@ -10,11 +11,22 @@ async fn say_hi() -> &'static str {
     "HI!"
 }
 
+#[derive(Serialize)]
+struct HelloJSON {
+    message: String
+}
+
+async fn hello_json() -> axum::Json<HelloJSON> {
+    // axum::Json::from(...) also works
+    axum::Json(HelloJSON{message:"some data".to_string()})
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let app = Router::new()
         .route("/", get(say_hi))
-        .route("/html_hi", get(say_hello));
+        .route("/html_hi", get(say_hello))
+        .route("/json_hi", get(hello_json));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(listener, app).await?;
