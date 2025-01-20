c08b6f2c14ad6d7d6d987869257898ff34d9ad20
Omer Kaya <omerkaya513@gmail.com>
2025-01-20 22:04:58 +0200

    [add]: upload and download images funcs

--- course/fundamentals/module_three/thumbnail_service/src/main.rs
+++ course/fundamentals/module_three/thumbnail_service/src/main.rs
@@ -1,7 +1,8 @@
 #![allow(dead_code)]
 
-use axum::{response::Html, extract::Multipart, routing::{get, post}, Extension, Router};
-use sqlx::Row;
+use axum::{extract::{Multipart, Path}, body::Body, http::{header, HeaderMap}, response::{Html, IntoResponse}, routing::{get, post}, Extension, Router};
+use sqlx::{Row};
+use tokio_util::io::ReaderStream;
 
 async fn test(Extension(pool): Extension<sqlx::SqlitePool>) -> String {
     let result = sqlx::query("SELECT COUNT(id) FROM images")
@@ -18,14 +19,79 @@ async fn index_page() -> Html<String> {
     Html(content)
 }
 
-async fn uploader(mut multipart: Multipart) -> String {
+async fn insert_image(pool: &sqlx::SqlitePool, data: &str) -> anyhow::Result<i64> {
+    let row = sqlx::query("insert into images (tags) values (?) returning id")
+    .bind(data)
+    .fetch_one(pool)
+    .await?;
+
+    Ok(row.get(0))
+}
+
+async fn save_image(id: i64, bytes: &[u8]) -> anyhow::Result<()> {
+    // Check that the images folder exists and is a directory
+    // If it doesn't, create it.
+    let base_path = std::path::Path::new("images");
+    if !base_path.exists() || !base_path.is_dir() {
+        tokio::fs::create_dir_all(base_path).await?;
+    }
+
+    // Use "join" to create a path to the image file. Join is platform aware,
+    // it will handle the differences between Windows and Linux.
+    let image_path = base_path.join(format!("{id}.jpg"));
+    if image_path.exists() {
+        // The file exists. That shouldn't happen.
+        anyhow::bail!("File already exists");
+    }
+
+    // Write the image to the file
+    tokio::fs::write(image_path, bytes).await?;
+    Ok(())
+}
+
+async fn uploader(Extension(pool): Extension<sqlx::SqlitePool>, mut multipart: Multipart) -> String {
+    let mut tags = None;
+    let mut image = None;
     while let Some(field) = multipart.next_field().await.unwrap() {
         let name = field.name().unwrap().to_string();
         let data = field.bytes().await.unwrap();
 
         println!("{name} is {} bytes", data.len());
+
+        match name.as_str() {
+            "tags" => tags = Some(String::from_utf8(data.to_vec()).unwrap()),
+            "image" => image = Some(data.to_vec()),
+            _ => panic!("unknown field: {name}"),
+        }
+    }
+
+    if let (Some(tags), Some(image)) = (tags, image) {
+        let new_img_id = insert_image(&pool, &tags).await.unwrap();
+        save_image(new_img_id, &image).await.unwrap();
+    } else {
+        panic!("unknor or missing field");
     }
     "Ok".to_string()
+}
+
+async fn get_image(Path(id): Path<i64>) -> impl IntoResponse {
+    let filename = format!("images/{id}.jpg");
+    let attachment = format!("filename={filename}");
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        header::CONTENT_TYPE,
+        header::HeaderValue::from_static("image/jpeg"),
+    );
+    headers.insert(
+        header::CONTENT_DISPOSITION,
+        header::HeaderValue::from_str(&attachment).unwrap()
+    );
+    let file = tokio::fs::File::open(&filename).await.unwrap();
+    axum::response::Response::builder()
+        .header(header::CONTENT_TYPE, header::HeaderValue::from_static("image/jpeg"))
+        .header(header::CONTENT_DISPOSITION, header::HeaderValue::from_str(&attachment).unwrap())
+        .body(Body::from_stream(ReaderStream::new(file)))
+        .unwrap()
 }
 
 #[tokio::main]
@@ -42,6 +108,7 @@ async fn main() -> anyhow::Result<()> {
     .route("/", get(test))
     .route("/index", get(index_page))
     .route("/upload", post(uploader))
+    .route("/image/{id}", get(get_image))
     .layer(Extension(pool));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
