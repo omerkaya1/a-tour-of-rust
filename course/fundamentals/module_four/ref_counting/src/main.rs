d8a400602265a4035b19c1127f947d133afca071
Omer Kaya <omerkaya513@gmail.com>
2025-01-22 12:41:51 +0200

    [add]: internal mutability explored

--- course/fundamentals/module_four/ref_counting/src/main.rs
+++ course/fundamentals/module_four/ref_counting/src/main.rs
@@ -1,65 +1,96 @@
-use std::sync::Arc;
+// use std::sync::Arc;
 
-#[derive(Debug)]
-struct Droppable(i32);
+// #[derive(Debug)]
+// struct Droppable(i32);
 
-impl Droppable {
-    fn new(n: i32) -> Self {
-        println!("Constructing {n}");
-        Self(n)
+// impl Droppable {
+//     fn new(n: i32) -> Self {
+//         println!("Constructing {n}");
+//         Self(n)
+//     }
+// }
+
+// impl Drop for Droppable {
+//     fn drop(&mut self) {
+//         println!("Dropping {}", self.0);
+//     }
+// }
+
+// fn move_me(x: Arc<Droppable>) {
+//     println!("Moved {}", x.0);
+// }
+
+// // struct SharedData(String);
+
+// fn main() {
+//     // let my_shared = Arc::new(Droppable::new(1));
+//     // {
+//     //     let _x = my_shared.clone();
+//     //     let _y = my_shared.clone();
+//     //     let _z = my_shared.clone();
+//     // }
+//     // move_me(my_shared.clone());
+
+//     // let mut threads = Vec::new();
+//     // for _ in 0..10 {
+//     //     let my_clone = my_shared.clone();
+//     //     threads.push(std::thread::spawn(move || {
+//     //         println!("{my_clone:?}");
+//     //     }));
+//     // }
+
+//     // for t in threads {
+//     //     t.join().unwrap();
+//     // }
+
+//     // println!("{my_shared:?}");
+//     // println!("Application exit");
+
+//     // use std::sync::{Arc, Mutex};
+
+//     // let my_shared = Arc::new(Mutex::new(SharedData("Hello".to_string())));
+//     // let mut threads = Vec::new();
+//     // for i in 0..10 {
+//     //     let my_shared = my_shared.clone();
+//     //     threads.push(std::thread::spawn(move || {
+//     //         let mut data = my_shared.lock().unwrap();
+//     //         data.0.push_str(&format!(" {i}"));
+//     //     }));
+//     // }
+//     // for t in threads {
+//     //     t.join().unwrap();
+//     // }
+//     // let data = my_shared.lock().unwrap();
+//     // println!("{}", data.0);
+// }
+
+use std::sync::{Arc, Mutex};
+
+struct SharedData{
+    data: Mutex<String>,
+}
+
+impl SharedData {
+    fn new(s: &str) -> Self {
+        Self {
+            data: Mutex::new(s.to_string()),
+        }
     }
 }
-
-impl Drop for Droppable {
-    fn drop(&mut self) {
-        println!("Dropping {}", self.0);
-    }
-}
-
-fn move_me(x: Arc<Droppable>) {
-    println!("Moved {}", x.0);
-}
-
-struct SharedData(String);
 
 fn main() {
-    // let my_shared = Arc::new(Droppable::new(1));
-    // {
-    //     let _x = my_shared.clone();
-    //     let _y = my_shared.clone();
-    //     let _z = my_shared.clone();
-    // }
-    // move_me(my_shared.clone());
-
-    // let mut threads = Vec::new();
-    // for _ in 0..10 {
-    //     let my_clone = my_shared.clone();
-    //     threads.push(std::thread::spawn(move || {
-    //         println!("{my_clone:?}");
-    //     }));
-    // }
-
-    // for t in threads {
-    //     t.join().unwrap();
-    // }
-
-    // println!("{my_shared:?}");
-    // println!("Application exit");
-
-    use std::sync::{Arc, Mutex};
-
-    let my_shared = Arc::new(Mutex::new(SharedData("Hello".to_string())));
+    let my_shared = Arc::new(SharedData::new("Hello"));
     let mut threads = Vec::new();
     for i in 0..10 {
         let my_shared = my_shared.clone();
         threads.push(std::thread::spawn(move || {
-            let mut data = my_shared.lock().unwrap();
-            data.0.push_str(&format!(" {i}"));
+            let mut data = my_shared.data.lock().unwrap();
+            data.push_str(&format!(" {i}"));
         }));
     }
     for t in threads {
         t.join().unwrap();
     }
-    let data = my_shared.lock().unwrap();
-    println!("{}", data.0);
+    let data = my_shared.data.lock().unwrap();
+    println!("{data}");
 }
