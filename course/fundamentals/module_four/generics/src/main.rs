8f44aa071d3d13211f25667e88ef813cc393a237
Omer Kaya <omerkaya513@gmail.com>
2025-01-23 13:51:09 +0200

    [add]: generic implementation for hash map buckets

--- course/fundamentals/module_four/generics/src/main.rs
+++ course/fundamentals/module_four/generics/src/main.rs
@@ -11,30 +11,59 @@
 //     println!("{}", x.to_string())
 // }
 
-struct Degrees(f32);
-struct Radians(f32);
+// struct Degrees(f32);
+// struct Radians(f32);
 
-impl From<Radians> for Degrees {
-    fn from(rad: Radians) -> Self {
-        Degrees(rad.0 * 180.0 / std::f32::consts::PI)
-    }
+// impl From<Radians> for Degrees {
+//     fn from(rad: Radians) -> Self {
+//         Degrees(rad.0 * 180.0 / std::f32::consts::PI)
+//     }
+// }
+
+// impl From<Degrees> for Radians {
+//     fn from(deg: Degrees) -> Self {
+//         Radians(deg.0 * std::f32::consts::PI / 180.0)
+//     }
+// }
+
+// fn sin(angle: impl Into<Radians>) -> f32 {
+//     let angle: Radians = angle.into();
+//     angle.0.sin()
+// }
+
+// fn main() {
+//     let behind_you = Degrees(180.0);
+//     let behind_you_radians = Radians::from(behind_you);
+//     let behind_you_radians2: Radians = Degrees(180.0).into();
+
+//     println!("{}", sin(behind_you_radians2));
+// }
+
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct HashMapBucket<K, V> {
+    map: HashMap<K, Vec<V>>,
 }
 
-impl From<Degrees> for Radians {
-    fn from(deg: Degrees) -> Self {
-        Radians(deg.0 * std::f32::consts::PI / 180.0)
+impl <K, V> HashMapBucket<K, V> 
+where K: Eq + std::hash::Hash
+{    
+    fn new() -> Self {
+        HashMapBucket { map: HashMap::new() }
     }
-}
 
-fn sin(angle: impl Into<Radians>) -> f32 {
-    let angle: Radians = angle.into();
-    angle.0.sin()
+    fn insert(&mut self, key: K, val: V) {
+        let values = self.map.entry(key).or_insert(Vec::new());
+        values.push(val);
+    }
 }
 
 fn main() {
-    let behind_you = Degrees(180.0);
-    let behind_you_radians = Radians::from(behind_you);
-    let behind_you_radians2: Radians = Degrees(180.0).into();
+    let mut bucket = HashMapBucket::new();
+    bucket.insert("key", 123);
+    bucket.insert("yek", 321);
+    bucket.insert("kye", 111);
 
-    println!("{}", sin(behind_you_radians2));
+    println!("{:?}", bucket);
 }
