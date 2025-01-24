b6fc291da49bee97c18079b7ccf5ec97975d1b38
Omer Kaya <omerkaya513@gmail.com>
2025-01-24 12:46:22 +0200

    [add]: implementing a protocol

--- course/fundamentals/module_four/bytes/src/main.rs
+++ course/fundamentals/module_four/bytes/src/main.rs
@@ -1,25 +1,72 @@
 #![allow(dead_code)]
+
 #[repr(C)]
-#[derive(bytemuck::Zeroable, bytemuck::Pod, Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 struct Data {
     num: u16,
-    tag: [u8; 8],
+    tag: String,
 }
 
-fn main() -> anyhow::Result<()> {
-    let some = vec![
-        Data{num: 1, tag: *b"Hello   ",},
-        Data{num: 2, tag: *b"data!   ",},
-    ];
+// fn main() -> anyhow::Result<()> {
+//     let some = vec![
+//         Data{num: 1, tag: *b"Hello   ",},
+//         Data{num: 2, tag: *b"data!   ",},
+//     ];
 
-    let bytes: &[u8] = bytemuck::cast_slice(&some);
-    std::fs::write("data.bin", bytes)?;
+//     let bytes: &[u8] = bytemuck::cast_slice(&some);
+//     std::fs::write("data.bin", bytes)?;
 
-    // read the data from file
-    let b = std::fs::read("data.bin").unwrap();
-    let d: &[Data] = bytemuck::cast_slice(&b);
+//     // read the data from file
+//     let b = std::fs::read("data.bin").unwrap();
+//     let d: &[Data] = bytemuck::cast_slice(&b);
 
-    println!("{d:?}");
+//     println!("{d:?}");
 
-    Ok(())
+//     Ok(())
+// }
+
+use std::{fs::File, io::Write};
+
+struct OurData {
+    number: u16,
+    tag: String,
+}
+
+fn main() {
+    let a = OurData {
+        number: 12,
+        tag: "Hello World".to_string(),
+    };
+
+    // Write the record in parts
+    let mut file = File::create("bytes.bin").unwrap();
+
+    // Write the number and check that 2 bytes were written
+    assert_eq!(file.write(&a.number.to_le_bytes()).unwrap(), 2);
+
+    // Write the string length IN BYTES and check that 8 bytes were written
+    let len = a.tag.as_bytes().len();
+    assert_eq!(file.write(&(len as u64).to_le_bytes()).unwrap(), 8);
+
+    // Write the string and check that the correct number of bytes were written
+    assert_eq!(file.write(a.tag.as_bytes()).unwrap(), len);
+
+    ///// READ THE DATA BACK
+    // Read the whole file as bytes.
+    let bytes = std::fs::read("bytes.bin").unwrap();
+
+    // Read the number
+    let number = u16::from_le_bytes(bytes[0..2].try_into().unwrap());
+
+    // Read the string length
+    let length = u64::from_le_bytes(bytes[2..10].try_into().unwrap());
+
+    // Decode the string
+    let tag = std::str::from_utf8(&bytes[10..(10 + length as usize)]).unwrap();
+
+    let a = Data {
+        num: number,
+        tag: tag.to_string(),
+    };
+    println!("{a:?}");
 }
