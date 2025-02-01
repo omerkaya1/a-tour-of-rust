8bd9ccbe37716ab3fe3f78f420e2361658cd005a
Omer Kaya <omerkaya513@gmail.com>
2025-02-01 18:03:54 +0200

    [add]: documentation explored

---
+++ course/best-practices/docs/src/main.rs
@@ -0,0 +1,16 @@
+#![warn(missing_docs)]
+
+
+
+//! # Example module
+//! 
+//! this module demonstrates rust documentation system
+//! 
+//! ## Examples:
+//! 
+//! use some::some_fn;
+//! 
+//! some_fn();
+fn main() {
+    println!("Hello, world!");
+}
--- course/best-practices/format/src/main.rs
+++ course/best-practices/format/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
+
+    #[rustfmt::skip]
+    mod section {
+        const n: i32 = 1;
+    }
 }
