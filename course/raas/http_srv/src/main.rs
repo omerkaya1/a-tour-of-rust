c921fee73034efe13f5497aa7598d575b03c8179
Omer Kaya <omerkaya513@gmail.com>
2025-02-04 18:48:41 +0200

    [raas]: added path extractor

--- course/raas/http_srv/src/main.rs
+++ course/raas/http_srv/src/main.rs
@@ -1,14 +1,22 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{response::Html, routing::get, Router, extract::Path};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+    .route("/", get(handler))
+    .route("/book/{id}", get(handler));
 
-    let listener = tokio::net::TcpListener::bind("").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello!</h1>")
+}
+
+// path extraction logic
+async fn path_extract(Path(id): Path<u32>) -> Html<String> {
+    println!("{id}");
+    Html(format!("id passed: {id}"))
 }
