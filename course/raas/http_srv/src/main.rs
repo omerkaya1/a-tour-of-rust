e0c1d848449487a04b53fe49e2b861e99b8422de
Omer Kaya <omerkaya513@gmail.com>
2025-02-04 18:54:33 +0200

    [raas]: header data extraction explored

--- course/raas/http_srv/src/main.rs
+++ course/raas/http_srv/src/main.rs
@@ -1,13 +1,15 @@
 use std::collections::HashMap;
 
 use axum::{extract::Path, extract::Query, response::Html, routing::get, Router};
+use axum::http::HeaderMap;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(handler))
         .route("/book/{id}", get(path_extract))
-        .route("/book", get(query_extract));
+        .route("/book", get(query_extract))
+        .route("/header", get(header_extract));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -28,5 +30,10 @@ async fn path_extract(Path(id): Path<u32>) -> Html<String> {
 
 // query extraction logic
 async fn query_extract(Query(params): Query<HashMap<String, String>>) -> Html<String> {
-    Html(format!("params passed: {params:#?}"))
+    Html(format!("query params passed: {params:#?}"))
+}
+
+// header extraction logic
+async fn header_extract(headers: HeaderMap) -> Html<String> {
+    Html(format!("headers passed: {headers:#?}"))
 }
