e5f50e8657c55c3c2af73166ae02893794dd4d7c
Omer Kaya <omerkaya513@gmail.com>
2025-02-04 18:51:45 +0200

    [raas]: query params extraction explored

--- course/raas/http_srv/src/main.rs
+++ course/raas/http_srv/src/main.rs
@@ -1,12 +1,17 @@
-use axum::{response::Html, routing::get, Router, extract::Path};
+use std::collections::HashMap;
+
+use axum::{extract::Path, extract::Query, response::Html, routing::get, Router};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-    .route("/", get(handler))
-    .route("/book/{id}", get(handler));
+        .route("/", get(handler))
+        .route("/book/{id}", get(path_extract))
+        .route("/book", get(query_extract));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
@@ -19,4 +24,9 @@ async fn handler() -> Html<&'static str> {
 async fn path_extract(Path(id): Path<u32>) -> Html<String> {
     println!("{id}");
     Html(format!("id passed: {id}"))
+}
+
+// query extraction logic
+async fn query_extract(Query(params): Query<HashMap<String, String>>) -> Html<String> {
+    Html(format!("params passed: {params:#?}"))
 }
