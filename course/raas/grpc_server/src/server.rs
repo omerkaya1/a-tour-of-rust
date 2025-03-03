7c69096888c8f215ac73427199293259991aedf2
Omer Kaya <omerkaya513@gmail.com>
2025-03-03 23:46:17 +0200

    [raas]: tracing interceptor

--- course/raas/grpc_server/src/server.rs
+++ course/raas/grpc_server/src/server.rs
@@ -9,12 +9,15 @@ pub mod hello_module {
 use hello_module::greeter_server::{Greeter, GreeterServer};
 use hello_module::{HelloRequest, HelloResponse};
 
+use tracing_subscriber::fmt::format::FmtSpan;
+
 #[derive(Debug, Default)]
 pub struct MyGreeter {}
 
 #[tonic::async_trait]
 impl Greeter for MyGreeter {
     // NOTE: needs to be exactly the same name as it was defined in the proto file (the service section)
+    #[tracing::instrument]
     async fn hello(&self, req: Request<HelloRequest>) -> Result<Response<HelloResponse>, Status> {
         let resp = hello_module::HelloResponse {
             message: format!("hello {}", req.into_inner().name),
@@ -36,10 +39,24 @@ fn auth(req: Request<()>) -> Result<Request<()>, Status> {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // add tracing subscriber
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_target(false)
+        .with_span_events(FmtSpan::CLOSE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)?;
+
     let addr = "[::1]:50051".parse()?;
     let greeter = MyGreeter::default();
 
     Server::builder()
+        .trace_fn(|_| tracing::info_span!("grpc-server"))
         .add_service(GreeterServer::with_interceptor(greeter, auth))
         .serve(addr)
         .await?;
